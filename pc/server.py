4678211b8b01e14b1646f612793c9792cf41a322
jiangxiaoma111 <369806726@qq.com>
2014-11-10 23:47:21 +0800

    just modify it

--- pc/server.py
+++ pc/server.py
@@ -1,10 +1,11 @@
-# /usr/local/bin/python3
+#! /usr/local/bin/python3
 import socket
 import os
 
 SHUTDOWN = 0x01
-HALT = 0x02
-REBOOT = 0x03
+SUSPEND  = 0x02
+REBOOT   = 0x03
+LOGOUT   = 0x04
 
 def shutdown():
 	if 'nt' == os.name:
@@ -12,17 +13,23 @@ def shutdown():
 	elif 'posix' == os.name:
 		return os.system('shutdown -P +0')
 
-def halt():
+def suspend():
 	if 'nt' == os.name:
-		return os.system('shutdown ')
+		return os.system('rundll32.exe powrprof.dll,SetSuspendState 0,1,0')
 	elif 'posix' == os.name:
-		return os.system('shutdown -H +0')
+		return os.system('pm-suspend')
 
 def reboot():
 	if 'nt' == os.name:
-		return os.system('shutdown')
+		return os.system('shutdown /r /t 0')
 	elif 'posix' == os.name:
-		return os.system('shutdown -r')
+		return os.system('shutdown -r +0')
+
+def logout():
+	if 'nt' == os.name:
+		return os.system('shutdown /l')
+	elif 'posix' == os.name:
+		return os.system('shutdown')
 HOST = ''
 PORT = 50007
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,10 +41,16 @@ def recvcmd():
 	ret = 1000
 	if command[0] == SHUTDOWN:
 		ret = shutdown()
-	print(ret)
-	return ret
+	elif SUSPEND == command[0]:
+		ret = suspend()
+	elif REBOOT == command[0]:
+		ret = reboot()
+	elif LOGOUT == command[0]:
+		ret = logout()
+	return ret, addr
 stat = 1000
+addr = None
 while stat is not 0:
-	stat = recvcmd()
+	stat, addr = recvcmd()
 ret = int.to_bytes(stat, 1, 'big', signed=True)
 server.sendto(ret, addr)
