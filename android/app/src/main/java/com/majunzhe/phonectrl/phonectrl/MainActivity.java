d40df37ba0d041a955f93bf1d7e15beff5cc5c28
jiangxiaoma111 <369806726@qq.com>
2014-11-12 22:25:06 +0800

    support suspend logout reboot

--- android/app/src/main/java/com/majunzhe/phonectrl/phonectrl/MainActivity.java
+++ android/app/src/main/java/com/majunzhe/phonectrl/phonectrl/MainActivity.java
@@ -13,6 +13,9 @@ import module.Command;
 public class MainActivity extends Activity {
 
     private Button btnShutdown;
+    private Button btnSuspend;
+    private Button btnReboot;
+    private Button btnLogout;
     private Command command;
 
     @Override
@@ -21,21 +24,58 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         btnShutdown = (Button)findViewById(R.id.btn_shutdown);
+        btnSuspend = (Button)findViewById(R.id.btn_suspend);
+        btnReboot = (Button)findViewById(R.id.btn_reboot);
+        btnLogout = (Button)findViewById(R.id.btn_logout);
         command = new Command();
 
         btnShutdown.setOnClickListener(btnListener);
+        btnSuspend.setOnClickListener(btnListener);
+        btnReboot.setOnClickListener(btnListener);
+        btnLogout.setOnClickListener(btnListener);
     }
 
     private View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Thread th;
             switch (v.getId()){
                 case R.id.btn_shutdown:
-                    Thread th = new Thread() {
+                    th = new Thread() {
                         @Override
                         public void run() {
                             super.run();
                             command.shutdown();
+                        }
+                    };
+                    th.start();
+                    break;
+                case R.id.btn_suspend:
+                    th = new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            command.suspend();
+                        }
+                    };
+                    th.start();
+                    break;
+                case R.id.btn_reboot:
+                    th = new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            command.reboot();
+                        }
+                    };
+                    th.start();
+                    break;
+                case R.id.btn_logout:
+                    th = new Thread() {
+                        @Override
+                        public void run() {
+                            super.run();
+                            command.logout();
                         }
                     };
                     th.start();
